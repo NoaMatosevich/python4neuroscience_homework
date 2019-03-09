1966f4b9a3c1daee9bc62a3b801a8578a1877515
NoaMatosevich <48092776+NoaMatosevich@users.noreply.github.com>
2019-03-09 17:03:22 +0200

    Update assignment1_part3.py

--- assignment1/assignment1_part3.py
+++ assignment1/assignment1_part3.py
@@ -1,4 +1,5 @@
-def compare_subjects_within_student(subj1, subj2):
+def compare_subjects_within_student(subj1_all_students,
+                                     subj2_all_students):
     """
     Compare the two subjects with their students and print out the "preferred"
     subject for each student. Single-subject students shouldn't be printed.
@@ -6,29 +7,32 @@ def compare_subjects_within_student(subj1, subj2):
     Choice for the data structure of the function's arguments is up to you.
     """
     # turn each dictionary to a dict containing mean scores for each student    
-    for n in subj1:
-        subj1[n]=(subj1[n][1]+subj1[n][0])/2.0
-    for m in subj2:
-        subj2[m]=(subj2[m][1]+subj2[m][0])/2.0
+    for n in subj1_all_students:
+        if n=='subject_name':
+            continue
+        subj1_all_students[n]=(subj1_all_students[n][1]+subj1_all_students[n][0])/2.0
+    for m in subj2_all_students:
+        if m=='subject_name':
+            continue
+        subj2_all_students[m]=(subj2_all_students[m][1]+subj2_all_students[m][0])/2.0
+    # the result dictionary contains the better subject next to each student name
     result_dic={}
-    for i in subj1:
-        try:
-            if subj2[i]>subj1[i]:
-                result_dic[i]='subj2'
-            elif subj2[i]<subj1[i]:
-                result_dic[i]='subj1'
-        except:
+    for i in subj1_all_students:
+        if i=='subject_name':
+            continue
+        try: #the try command allows to test the possibility that there is no corresponding student name in subject 2.
+            if subj2_all_students[i]>subj1_all_students[i]:
+                result_dic[i]=subj2_all_students['subject_name']
+            elif subj2_all_students[i]<subj1_all_students[i]:
+                result_dic[i]=subj1_all_students['subject_name']
+        except: #this will be triggered when there is no corresponding student name in the 2 lists, that name will be skipped. 
             continue
     return result_dic
     
+#each subject is a dictionary containing the subject name, and for each student name there is a 
+# tupple containing the 2 test scores
 
-
-art={'Yoni':(80, 67),'Maya':(92, 100), 'Shira':(97, 88), 'Moo':(79, 68), 'Dana':(65,87)}
-physics={'Moo':(97, 56),'Maya':(63, 92),'Shira':(99, 75), 'Yoni':(96, 100), 'Yossi':(98, 87)}
+art={'subject_name':'art', 'Yoni':(80, 67),'Maya':(92, 100), 'Shira':(97, 88), 'Moo':(79, 68), 'Dana':(65,87)}
+physics={'subject_name':'physics', 'Moo':(97, 56),'Maya':(63, 92),'Shira':(99, 75), 'Yoni':(96, 100), 'Yossi':(98, 87)}
 results=compare_subjects_within_student(art, physics)
-for k in results:
-    if results[k]=='subj1':
-        results[k]='art'
-    else:
-        results[k]='physics'
 print(results)
