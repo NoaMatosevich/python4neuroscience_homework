1eaa9fff54f0c1de554ff556211852f247584442
NoaMatosevich <48092776+NoaMatosevich@users.noreply.github.com>
2019-03-10 19:19:07 +0200

    first homework assignment

--- assignment1/hw1_question3.py
+++ assignment1/hw1_question3.py
@@ -10,16 +10,16 @@ def compare_subjects_within_student(subj1_all_students,
     for n in subj1_all_students:
         if n=='subject_name':
             continue
-        subj1_all_students[n]=(subj1_all_students[n][1]+subj1_all_students[n][0])/2.0
+        subj1_all_students[n]=max(subj1_all_students[n])
     for m in subj2_all_students:
         if m=='subject_name':
             continue
-        subj2_all_students[m]=(subj2_all_students[m][1]+subj2_all_students[m][0])/2.0
+        subj2_all_students[m]=max(subj2_all_students[m])
     # the result dictionary contains the better subject next to each student name
     result_dic={}
-    key_1=set(subj1_all_students.keys())
-    key_2=set(subj2_all_students.keys())
-    key_set=set.intersection(key_1,key_2) # the intersection command takes only the values that are common in both sets
+    key_1 = set(subj1_all_students.keys())
+    key_2 = set(subj2_all_students.keys())
+    key_set = set.intersection(key_1 , key_2) # the intersection command takes only the values that are common in both sets
     for i in key_set:
         if i=='subject_name': #skip the subject name
             continue
@@ -27,7 +27,6 @@ def compare_subjects_within_student(subj1_all_students,
                 result_dic[i]=subj2_all_students['subject_name']
         elif subj2_all_students[i]<subj1_all_students[i]: # average in subject 1 is higher
                 result_dic[i]=subj1_all_students['subject_name']
-        
     print(result_dic)
 
 
@@ -35,7 +34,7 @@ if __name__ == '__main__':
 # Question 3
 #each subject is a dictionary containing the subject name, and for each student name there is a 
 # tupple containing the 2 test scores
-    art={'subject_name':'art', 'Yoni':(80, 67),'Maya':(92, 100), 'Shira':(97, 88), 'Moo':(79, 68), 'Dana':(65,87)}
-    physics={'subject_name':'physics', 'Moo':(97, 56),'Maya':(63, 92),'Shira':(99, 75), 'Yoni':(96, 100), 'Yossi':(98, 87)}
+    art={'subject_name':'art', 'Yoni':(80, 67),'Maya':(92, 100), 'Shira':(97, 88), 'Miri':(79, 98), 'Dana':(65,87)}
+    physics={'subject_name':'physics', 'Miri':(97, 56),'Maya':(63, 92),'Shira':(99, 75), 'Yoni':(96, 100), 'Yossi':(98, 87)}
     print('Question 3 solution:')
     compare_subjects_within_student(art, physics)
